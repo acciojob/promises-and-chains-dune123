8a2365b3ccf4cb69f60d8feb9f628daa788c2a68
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-10 12:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const submitButton=document.getElementById("submitButton")
+
+//promise return answer
+const agePromise=(age,name)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(age>18){
+                resolve(`Welcome ${name}. You can vote`)
+            }
+            else{
+                reject(`Oh sorry ${name}. You aren't old enough`)
+            }
+        },4000)
+    })
+}
+
+submitButton.addEventListener("click",()=>{
+   const nameInput=document.getElementById("nameInput").value;
+   const numInput=document.getElementById("numInput").value;
+   if(nameInput.trim()===""||numInput.trim()===""){
+        alert("inputs cannot be empty")
+   }
+   else{
+        agePromise(Number(numInput),nameInput)
+        .then((data)=>{
+            alert(data);
+        })
+        .catch((err)=>{
+            alert(err)
+        })
+    }
+})
